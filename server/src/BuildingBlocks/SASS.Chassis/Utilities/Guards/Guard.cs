ffdd5fc3d0a6385e25e52f5a27c8c73860256b06
tuantai0708 <tai.bang157@gmail.com>
2026-03-15 16:07:37 +0700

    feat(guard): To add guard

---
+++ server/src/BuildingBlocks/SASS.Chassis/Utilities/Guards/Guard.cs
@@ -0,0 +1,8 @@
+﻿namespace SASS.Chassis.Utilities.Guards;
+
+public sealed class Guard
+{
+    private Guard() { }
+
+    public static Guard Against { get; } = new();
+}
---
+++ server/src/BuildingBlocks/SASS.Chassis/Utilities/Guards/GuardAgainstNotFoundExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SASS.Chassis.Exceptions;
+
+namespace SASS.Chassis.Utilities.Guards;
+
+public static class GuardAgainstNotFoundExtensions
+{
+    extension(Guard guard)
+    {
+        /// <summary>
+        /// Validates that the provided value is not null. If null, throws a <see cref="NotFoundException" />
+        /// for the specified <paramref name="id" />.
+        /// </summary>
+        /// <typeparam name="T">The type of the value being checked.</typeparam>
+        /// <param name="value">The value to check for null.</param>
+        /// <param name="id">The identifier associated with the entity being checked.</param>
+        /// <exception cref="NotFoundException">
+        /// Thrown when the value is null,
+        /// indicating that the entity with the specified <paramref name="id" /> was not found.
+        /// </exception>
+        public void NotFound<T>([NotNull] T? value, string id)
+        {
+            if (value is not null)
+            {
+                return;
+            }
+
+            throw NotFoundException.For<T>(id);
+        }
+
+        /// <summary>
+        /// Validates that the provided value is not null. If null, throws a <see cref="NotFoundException" />
+        /// for the specified <paramref name="id" />.
+        /// </summary>
+        /// <typeparam name="T">The type of the value being checked.</typeparam>
+        /// <param name="value">The value to check for null.</param>
+        /// <param name="id">The identifier associated with the entity being checked.</param>
+        /// <exception cref="NotFoundException">
+        /// Thrown when the value is null,
+        /// indicating that the entity with the specified <paramref name="id" /> was not found.
+        /// </exception>
+        public void NotFound<T>([NotNull] T? value, Guid id)
+        {
+            if (value is not null)
+            {
+                return;
+            }
+
+            throw NotFoundException.For<T>(id);
+        }
+    }
+}
