fd734cd0e059fdde56634c35ab47e69730a5e489
tuantai0708 <tai.bang157@gmail.com>
2026-03-15 15:56:38 +0700

    feat(chassis): To add mapper

---
+++ server/src/BuildingBlocks/SASS.Chassis/Mapper/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SASS.Chassis.Mapper;
+
+public static class Extensions
+{
+    public static void AddMapper(this IServiceCollection services, Type type)
+    {
+        services.Scan(scan =>
+            scan.FromAssemblies(type.Assembly)
+                .AddClasses(
+                    classes => classes.AssignableTo(typeof(IMapper<,>)).Where(t => !t.IsAbstract),
+                    false
+                )
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+        );
+    }
+}
